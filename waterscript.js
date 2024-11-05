151bcef1836aefcd471e663ac3ba85a1b33f3fee
Kim Massesson <150744968+Kima4Tec@users.noreply.github.com>
2024-11-05 10:39:57 +0100

    opdated waterProject

--- waterscript.js
+++ waterscript.js
@@ -3,6 +3,34 @@ const IP2=129;
 const IP3=33;
 const IP4=45;
 
+const bits = [
+    document.getElementById('bit1'),
+    document.getElementById('bit2'),
+    document.getElementById('bit3'),
+    document.getElementById('bit4'),
+    document.getElementById('bit5'),
+    document.getElementById('bit6'),
+    document.getElementById('bit7'),
+    document.getElementById('bit8')
+];
+
+const ipAddressDiv = document.querySelector(".ipAddress");
+
+function updateIPAddress() {
+    let binaryValue = '';
+    bits.forEach(bit => {
+        binaryValue += bit.value === '1' ? '1' : '0';
+    });
+
+    const decimalValue = parseInt(binaryValue, 2);
+
+    ipAddressDiv.innerHTML = `= ${decimalValue}`;
+}
+
+bits.forEach(bit => {
+    bit.addEventListener('input', updateIPAddress);
+});
+
 function getCode (){
     userIP1 = parseInt(document.querySelector('#firstIP').value)
     userIP2 = parseInt(document.querySelector('#secondIP').value)
@@ -10,10 +38,29 @@ function getCode (){
     userIP4 = parseInt(document.querySelector('#fourthIP').value)
 if (IP1 === userIP1 && IP2 === userIP2 && IP3 === userIP3 && IP4 === userIP4)
     {
+        document.querySelector('.getRegulatorCode').style.display = "none";
+        document.querySelector('.input-regulator-code').style.display = "inline";
         document.querySelector('#answer').innerHTML = "I fandt den rigtige PC og får koden. Koden er: 7QRS2"
     } else {
         document.querySelector('#answer').innerHTML = "IP-adressen er forkert!"
     }
+}
 
+function showRegulator() {
+    if(document.querySelector("#regulatorPW").innerHTML = "7QRS2")
+    document.querySelector('.input-regulator-code').style.display = "none";   
+    document.querySelector('.regulator').style.display = "flex";
+}
+
+function regulate(number) {
+    presentPH = parseInt(document.querySelector("#phValue").innerHTML);
+    console.log(presentPH) 
+presentPH = presentPH + number
+    showPH(presentPH)
+}
+function showPH (phNumber) {
+    document.querySelector("#phValue").innerHTML = phNumber;
+    if(phNumber === 7)
+        document.querySelector('.congrats').style.display = "inline";
 }
 
